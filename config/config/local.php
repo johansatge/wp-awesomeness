6ca2848e42ce657f34446b2462c958683327ec88
Johan <contact@johansatge.fr>
2015-07-07 19:18:04 +0200

    MySQL config

--- config/config/local.php
+++ config/config/local.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'mysql');
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
