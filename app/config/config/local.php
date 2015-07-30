b0e006300d79a6bace218d82489b031caf47679b
Johan Satgé <contact@johansatge.fr>
2015-07-30 10:50:00 +0200

    Updates local config

--- app/config/config/local.php
+++ app/config/config/local.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'mysql');
 define('DB_NAME', 'wp_bootstrap');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('WPLANG', 'fr_FR');
+define('WPLANG', 'en_US');
 define('WP_DEBUG', true);
 //define('WP_CACHE', true);
